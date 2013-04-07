a53881856fc24c8f1adf25a2283eb3237f17934d
Patricio Beltran <pbelsal@gmail.com>
2013-04-07 10:27:10 -0500

    Fix browsing error

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,6 @@ class ItemsController < ApplicationController
   # POST /items.json
   def create
     @item = Item.new(params[:item])
-
     respond_to do |format|
       if @item.save
         format.html { redirect_to items_path, notice: 'Item was successfully created.' }
